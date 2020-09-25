ca21c06a9fdadc71b3b5fdfd01bad0d92815b08a
Azamat Sabyrbayev <sabyrbaev_azamat@mail.ru>
2020-09-25 08:14:37 +0600

    Use registers from r4 to r10

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -8,47 +8,40 @@
 
 .global count_det
 count_det:
-    ldr r2, [r0]        @ r2 = a
-    ldr r3, [r0, #16]   @ r3 = e
-    mul r2, r2, r3      @ r2 = a * e
-    ldr r3, [r0, #32]   @ r3 = i
-    mul r2, r2, r3      @ r2 = a * e * i
-    mov r1, r2          @ r1 = a * e * i
+    push {r4-r10}
+    ldr r2, [r0]            @ r2 = a
+    ldr r3, [r0, #4]        @ r3 = b
+    ldr r4, [r0, #8]        @ r4 = c
+    ldr r5, [r0, #12]       @ r5 = d
+    ldr r6, [r0, #16]       @ r6 = e
+    ldr r7, [r0, #20]       @ r7 = f
+    ldr r8, [r0, #24]       @ r8 = g
+    ldr r9, [r0, #28]       @ r9 = h
+    ldr r10, [r0, #32]      @ r10 = i
 
-    ldr r2, [r0, #4]    @ r2 = b
-    ldr r3, [r0, #20]   @ r3 = f
-    mul r2, r2, r3      @ r2 = b * f
-    ldr r3, [r0, #24]   @ r3 = g
-    mul r2, r2, r3      @ r2 = b * f * g
-    add r1, r1, r2      @ r1 = (a * e * i) + (b * f * g)
+    mul r1, r2, r6          @ r2 = a * e
+    mul r1, r1, r10         @ r2 = a * e * i
+    mov r0, r1              @ r0 = a * e * i
 
-    ldr r2, [r0, #8]    @ r2 = c
-    ldr r3, [r0, #12]   @ r3 = d
-    mul r2, r2, r3      @ r2 = c * d
-    ldr r3, [r0, #28]   @ r3 = h
-    mul r2, r2, r3      @ r2 = c * d * h
-    add r1, r1, r2      @ r1 = (a * e * i) + (b * f * g) + (c * d * h)
+    mul r1, r3, r7          @ r2 = b * f
+    mul r1, r1, r8          @ r2 = b * f * g
+    add r0, r0, r1          @ r0 = (a * e * i) + (b * f * g)
+    
+    mul r1, r4, r5          @ r2 = c * d
+    mul r1, r1, r9          @ r2 = c * d * h
+    add r0, r0, r1          @ r0 = (a * e * i) + (b * f * g) + (c * d * h)
 
-    ldr r2, [r0, #8]    @ r2 = c
-    ldr r3, [r0, #16]   @ r3 = e
-    mul r2, r2, r3      @ r2 = c * e
-    ldr r3, [r0, #24]   @ r3 = g
-    mul r2, r2, r3      @ r2 = c * e * g
-    sub r1, r1, r2      @ r1 = (a * e * i) + (b * f * g) + (c * d * h) - (c * e * g)
+    mul r1, r4, r6          @ r2 = c * e
+    mul r1, r1, r8          @ r2 = c * e * g
+    sub r0, r0, r1          @ r0 = (a * e * i) + (b * f * g) + (c * d * h) - (c * e * g)
 
-    ldr r2, [r0, #4]    @ r2 = b
-    ldr r3, [r0, #12]   @ r3 = d
-    mul r2, r2, r3      @ r2 = b * d
-    ldr r3, [r0, #32]   @ r3 = i
-    mul r2, r2, r3      @ r2 = b * d * i
-    sub r1, r1, r2      @ r1 = (a * e * i) + (b * f * g) + (c * d * h) - (c * e * g) - (b * d * i)
+    mul r1, r3, r5          @ r2 = b * d
+    mul r1, r1, r10         @ r2 = b * d * i
+    sub r0, r0, r1          @ r0 = (a * e * i) + (b * f * g) + (c * d * h) - (c * e * g) - (b * d * i)
 
-    ldr r2, [r0]        @ r2 = a
-    ldr r3, [r0, #20]   @ r3 = f
-    mul r2, r2, r3      @ r2 = a * f
-    ldr r3, [r0, #28]   @ r3 = h
-    mul r2, r2, r3      @ r2 = a * f * h
-    sub r1, r1, r2      @ r1 = (a * e * i) + (b * f * g) + (c * d * h) - (c * e * g) - (b * d * i) - (a * f * h)
+    mul r1, r2, r7          @ r2 = a * f
+    mul r1, r1, r9          @ r2 = a * f * h
+    sub r0, r0, r1          @ r0 = (a * e * i) + (b * f * g) + (c * d * h) - (c * e * g) - (b * d * i) - (a * f * h)
 
-    mov r0, r1
+    pop {r4-r10}
     bx lr
