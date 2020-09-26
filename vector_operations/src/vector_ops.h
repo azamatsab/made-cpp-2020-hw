a0b7c3f74f08b5aab903e80d05583b005f0d3384
Azamat Sabyrbayev <sabyrbaev_azamat@mail.ru>
2020-09-26 13:46:47 +0600

    Remove spaces within < >

--- vector_operations/src/vector_ops.h
+++ vector_operations/src/vector_ops.h
@@ -10,38 +10,38 @@ using std::ostream;
 using std::istream;
 
 namespace task {
-  vector < double > operator + (const vector < double > & a,
-    const vector < double > & b) {
-    vector < double > summ;
+  vector <double> operator + (const vector <double> & a,
+    const vector <double> & b) {
+    vector <double> summ;
     for (size_t i = 0; i < a.size(); ++i) {
       summ.push_back(a[i] + b[i]);
     }
     return summ;
   }
 
-  vector < double > operator - (const vector < double > & a,
-    const vector < double > & b) {
-    vector < double > subs;
+  vector <double> operator - (const vector <double> & a,
+    const vector <double> & b) {
+    vector <double> subs;
     for (size_t i = 0; i < a.size(); ++i) {
       subs.push_back(a[i] - b[i]);
     }
     return subs;
   }
 
-  vector < double > operator + (const vector < double > & a) {
+  vector <double> operator + (const vector <double> & a) {
     return a;
   }
 
-  vector < double > operator - (const vector < double > & a) {
-    vector < double > inverse;
+  vector <double> operator - (const vector <double> & a) {
+    vector <double> inverse;
     for (size_t i = 0; i < a.size(); ++i) {
       inverse.push_back(-a[i]);
     }
     return inverse;
   }
 
-  double operator * (const vector < double > & a,
-    const vector < double > & b) {
+  double operator * (const vector <double> & a,
+    const vector <double> & b) {
     double dot_prod = 0;
     for (size_t i = 0; i < a.size(); ++i) {
       dot_prod += a[i] * b[i];
@@ -49,17 +49,17 @@ namespace task {
     return dot_prod;
   }
 
-  vector < double > operator % (const vector < double > & a,
-    const vector < double > & b) {
-    vector < double > vector_prod;
+  vector <double> operator % (const vector <double> & a,
+    const vector <double> & b) {
+    vector <double> vector_prod;
     vector_prod.push_back(a[1] * b[2] - a[2] * b[1]);
     vector_prod.push_back(a[2] * b[0] - a[0] * b[2]);
     vector_prod.push_back(a[0] * b[1] - a[1] * b[0]);
     return vector_prod;
   }
 
-  bool operator || (const vector < double > & a,
-    const vector < double > & b) {
+  bool operator || (const vector <double> & a,
+    const vector <double> & b) {
     double prev_ratio = 0;
     double ratio = 0;
     double eps = 1e-12;
@@ -75,8 +75,8 @@ namespace task {
     return true;
   }
 
-  bool operator && (const vector < double > & a,
-    const vector < double > & b) {
+  bool operator && (const vector <double> & a,
+    const vector <double> & b) {
     double prev_ratio = 0;
     double ratio = 0;
     double eps = 1e-12;
@@ -95,18 +95,18 @@ namespace task {
     return true;
   }
 
-  vector < int > operator | (const vector < int > & a,
-    const vector < int > & b) {
-    vector < int > c;
+  vector <int> operator | (const vector <int> & a,
+    const vector <int> & b) {
+    vector <int> c;
     for (size_t i = 0; i < a.size(); ++i) {
       c.push_back(a[i] | b[i]);
     }
     return c;
   }
 
-  vector < int > operator & (const vector < int > & a,
-    const vector < int > & b) {
-    vector < int > c;
+  vector <int> operator & (const vector <int> & a,
+    const vector <int> & b) {
+    vector <int> c;
     for (size_t i = 0; i < a.size(); ++i) {
       c.push_back(a[i] & b[i]);
     }
@@ -114,7 +114,7 @@ namespace task {
   }
 
   ostream & operator << (ostream & out,
-    const vector < double > & a) {
+    const vector <double> & a) {
     for (size_t i = 0; i < a.size(); ++i) {
       out << a[i] << " ";
     }
@@ -122,7 +122,7 @@ namespace task {
     return out;
   }
 
-  istream & operator >> (istream & in , vector < double > & a) {
+  istream & operator >> (istream & in , vector <double> & a) {
     size_t n; in >> n;
     a.clear();
     for (size_t i = 0; i < n; ++i) {
@@ -132,7 +132,7 @@ namespace task {
     return in;
   }
 
-  void reverse(vector < double > & a) {
+  void reverse(vector <double> & a) {
     size_t size = a.size();
     for (size_t i = 0; i < size / 2; ++i) {
       iter_swap(a.begin() + i, a.end() - i - 1);
