ee936edd21475dafa729ff0a3e57091cabb31a4c
Azamat Sabyrbayev <sabyrbaev_azamat@mail.ru>
2020-10-05 21:41:21 +0600

    Rename a nad b to opn1 and opn2

--- vector_operations/src/vector_ops.h
+++ vector_operations/src/vector_ops.h
@@ -10,60 +10,60 @@ using std::ostream;
 using std::istream;
 
 namespace task {
-  vector <double> operator + (const vector <double> & a,
-    const vector <double> & b) {
+  vector <double> operator + (const vector <double> & opn1,
+    const vector <double> & opn2) {
     vector <double> summ;
-    for (size_t i = 0; i < a.size(); ++i) {
-      summ.push_back(a[i] + b[i]);
+    for (size_t i = 0; i < opn1.size(); ++i) {
+      summ.push_back(opn1[i] + opn2[i]);
     }
     return summ;
   }
 
-  vector <double> operator - (const vector <double> & a,
-    const vector <double> & b) {
+  vector <double> operator - (const vector <double> & opn1,
+    const vector <double> & opn2) {
     vector <double> subs;
-    for (size_t i = 0; i < a.size(); ++i) {
-      subs.push_back(a[i] - b[i]);
+    for (size_t i = 0; i < opn1.size(); ++i) {
+      subs.push_back(opn1[i] - opn2[i]);
     }
     return subs;
   }
 
-  vector <double> operator + (const vector <double> & a) {
-    return a;
+  vector <double> operator + (const vector <double> & opn1) {
+    return opn1;
   }
 
-  vector <double> operator - (const vector <double> & a) {
+  vector <double> operator - (const vector <double> & opn1) {
     vector <double> inverse;
-    for (size_t i = 0; i < a.size(); ++i) {
-      inverse.push_back(-a[i]);
+    for (size_t i = 0; i < opn1.size(); ++i) {
+      inverse.push_back(-opn1[i]);
     }
     return inverse;
   }
 
-  double operator * (const vector <double> & a,
-    const vector <double> & b) {
+  double operator * (const vector <double> & opn1,
+    const vector <double> & opn2) {
     double dot_prod = 0;
-    for (size_t i = 0; i < a.size(); ++i) {
-      dot_prod += a[i] * b[i];
+    for (size_t i = 0; i < opn1.size(); ++i) {
+      dot_prod += opn1[i] * opn2[i];
     }
     return dot_prod;
   }
 
-  vector <double> operator % (const vector <double> & a,
-    const vector <double> & b) {
+  vector <double> operator % (const vector <double> & opn1,
+    const vector <double> & opn2) {
     vector <double> vector_prod;
-    vector_prod.push_back(a[1] * b[2] - a[2] * b[1]);
-    vector_prod.push_back(a[2] * b[0] - a[0] * b[2]);
-    vector_prod.push_back(a[0] * b[1] - a[1] * b[0]);
+    vector_prod.push_back(opn1[1] * opn2[2] - opn1[2] * opn2[1]);
+    vector_prod.push_back(opn1[2] * opn2[0] - opn1[0] * opn2[2]);
+    vector_prod.push_back(opn1[0] * opn2[1] - opn1[1] * opn2[0]);
     return vector_prod;
   }
 
-  bool operator || (const vector <double> & a,
-    const vector <double> & b) {
+  bool operator || (const vector <double> & opn1,
+    const vector <double> & opn2) {
     double eps = 1e-12;
-    for (size_t i = 0; i < a.size() - 1; ++i) {
-      double mul1 = a[i] * b[i + 1];  
-      double mul2 = a[i + 1] * b[i];  
+    for (size_t i = 0; i < opn1.size() - 1; ++i) {
+      double mul1 = opn1[i] * opn2[i + 1];
+      double mul2 = opn1[i + 1] * opn2[i];
       if (abs(mul1 - mul2) > eps) {
         return false;
       }
@@ -71,61 +71,61 @@ namespace task {
     return true;
   }
 
-  bool operator && (const vector <double> & a,
-    const vector <double> & b) {
-    if (a || b) {
-      for (size_t i = 0; i < a.size(); ++i) {
-        if ((a[i] < 0) && (b[i] >= 0) || (a[i] >= 0) && (b[i] < 0)) {
+  bool operator && (const vector <double> & opn1,
+    const vector <double> & opn2) {
+    if (opn1 || opn2) {
+      for (size_t i = 0; i < opn1.size(); ++i) {
+        if ((opn1[i] < 0) && (opn2[i] >= 0) || (opn1[i] >= 0) && (opn2[i] < 0)) {
           return false;
         }
       }
       return true;
-    } 
+    }
     return false;
   }
 
-  vector <int> operator | (const vector <int> & a,
-    const vector <int> & b) {
+  vector <int> operator | (const vector <int> & opn1,
+    const vector <int> & opn2) {
     vector <int> result_vector;
-    for (size_t i = 0; i < a.size(); ++i) {
-      result_vector.push_back(a[i] | b[i]);
+    for (size_t i = 0; i < opn1.size(); ++i) {
+      result_vector.push_back(opn1[i] | opn2[i]);
     }
     return result_vector;
   }
 
-  vector <int> operator & (const vector <int> & a,
-    const vector <int> & b) {
+  vector <int> operator & (const vector <int> & opn1,
+    const vector <int> & opn2) {
     vector <int> result_vector;
-    for (size_t i = 0; i < a.size(); ++i) {
-      result_vector.push_back(a[i] & b[i]);
+    for (size_t i = 0; i < opn1.size(); ++i) {
+      result_vector.push_back(opn1[i] & opn2[i]);
     }
     return result_vector;
   }
 
   ostream & operator << (ostream & out,
-    const vector <double> & a) {
-    for (size_t i = 0; i < a.size(); ++i) {
-      out << a[i] << " ";
+    const vector <double> & opn) {
+    for (size_t i = 0; i < opn.size(); ++i) {
+      out << opn[i] << " ";
     }
     out << endl;
     return out;
   }
 
-  istream & operator >> (istream & in , vector <double> & a) {
-    size_t n; 
+  istream & operator >> (istream & in , vector <double> & opn) {
+    size_t n;
     in >> n;
-    a.clear();
+    opn.clear();
     for (size_t i = 0; i < n; ++i) {
       double number; in >> number;
-      a.push_back(number);
+      opn.push_back(number);
     }
     return in;
   }
 
-  void reverse(vector <double> & a) {
-    size_t size = a.size();
+  void reverse(vector <double> & opn) {
+    size_t size = opn.size();
     for (size_t i = 0; i < size / 2; ++i) {
-      iter_swap(a.begin() + i, a.end() - i - 1);
+      iter_swap(opn.begin() + i, opn.end() - i - 1);
     }
   }
 
