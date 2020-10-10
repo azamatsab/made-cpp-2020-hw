d61fb34305c2dff6fa3ad62d30016ba23f4578fc
Azamat Sabyrbayev <sabyrbaev_azamat@mail.ru>
2020-10-10 13:35:44 +0600

    Add Line

--- geometry/src/geometry.h
+++ geometry/src/geometry.h
@@ -1,43 +1,69 @@
 #pragma once
-#include <cmath>
 
 struct Point {
  public:
   double x;
   double y;
-  
-  Point(double x, double y): x{x}, y{y} {}
 
-  double getX() const {
-      return x;
+  Point(double x, double y) : x{x}, y{y} {}
+
+  double getX() const { return x; }
+
+  double getY() const { return y; }
+
+  bool operator==(const Point &a) const {
+    if (a.getX() == x && a.getY() == y) {
+      return true;
+    }
+    return false;
   }
 
-  double getY() const {
-      return y;
-  }
-
-  bool operator==(const Point &a) {
-      if (a.getX() == x && a.getY() == y) {
-          return true;
-      }
+  bool operator!=(const Point &a) const {
+    if (a == *this) {
       return false;
-  }
-
-  bool operator!=(const Point &a) {
-      if (a.getX() != x || a.getY() != y) {
-          return true;
-      }
-      return false;
+    }
+    return true;
   }
 };
 
 class Line {
-    double angle_;
-    double bias_;
-    
-    Line(Point a, Point b) {
-        angle_ = atan2((b.getY() - a.getY()), (b.getX() - a.getX()));
+ public:
+  Line(Point a, Point b) {
+    angle_ = (b.getX() - a.getX()) / (b.getY() - a.getY());
+    bias_ = a.getY() - a.getX() * angle_;
+  }
+
+  Line(double a, double b) : angle_(a), bias_(b) {}
+
+  Line(Point a, double angle) {
+    angle_ = angle_;
+    bias_ = a.getY() - a.getX() * angle_;
+  }
+
+  double getAngle() const { return angle_; }
+
+  double getBias() const { return bias_; }
+
+  bool operator==(const Line &a) const {
+    if (a.getAngle() == angle_ && a.getBias() == bias_) {
+      return true;
     }
-    
-    Line(double a, double b): angle_(a), bias_(b) {}
+    return false;
+  }
+
+  bool operator!=(const Line &a) const {
+    if (a == *this) {
+      return false;
+    }
+    return true;
+  }
+
+ private:
+  double angle_;
+  double bias_;
+};
+
+class Shape {
+ public:
+  virtual Shape() {}
 };
