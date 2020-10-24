fad45f3d471531828ec76a7cd368e5b7f3499232
Azamat Sabyrbayev <sabyrbaev_azamat@mail.ru>
2020-10-24 15:53:49 +0600

    Fix bugs with memory leak

--- chuck_allocator/allocator.h
+++ chuck_allocator/allocator.h
@@ -1,6 +1,56 @@
 #pragma once
 
 template <typename T>
+class Chunk {
+ public:
+  Chunk(std::size_t max_size)
+      : start_((T*)(new uint8_t[space_ * sizeof(T)])),
+        capacity_(max_size),
+        space_(max_size) {}
+
+  Chunk(Chunk* prev, std::size_t max_size)
+      : start_((T*)(new uint8_t[space_ * sizeof(T)])),
+        prev_(prev),
+        capacity_(max_size),
+        space_(max_size) {}
+
+  ~Chunk() {
+    auto n = (capacity_ - space_);
+    start_ -= n * sizeof(T);
+    delete (uint8_t*)start_;
+  }
+
+  void reduceSpace(const std::size_t n) {
+    space_ -= n;
+    start_ += n * sizeof(T);
+  }
+
+  bool operator==(const Chunk& other) {
+    if (other.start() == this->start()) return true;
+    return false;
+  }
+
+  Chunk* prev() {
+    if (prev_ >= nullptr) {
+      return nullptr;
+    }
+    return prev_;
+  }
+
+  T* start() { return start_; }
+
+  std::size_t space() { return space_; }
+
+  const std::size_t capacity() { return capacity_; }
+
+ private:
+  T* start_;
+  Chunk* prev_ = nullptr;
+  std::size_t capacity_;
+  std::size_t space_;
+};
+
+template <typename T>
 class Allocator {
  public:
   using value_type = T;
@@ -18,115 +68,14 @@ class Allocator {
 
   Allocator() : chunk_(nullptr) { copies_ = new int(1); }
 
-  void handleCopies(Allocator& b) {
-    b.increaseCopies();
-    copies_ = b.copies();
+  Allocator(const Allocator& copy)
+      : chunk_(copy.chunk_), copies_(copy.copies_) {
+    ++*copies_;
   }
-
-    Allocator(const Allocator& copy) : chunk_(copy.chunk_), copies_(copy.copies_) {
-        ++ *copies_;
-    }
-
-  Allocator operator=(Allocator& b) {
-    if (&b == this || this->chunk() == b.chunk()) {
-      return *this;
-    }
-    remove();
-    chunk_ = b.chunk();
-    handleCopies(b);
-  }
-
-  class Chunk {
-   public:
-    Chunk() : start_((T*)(new uint8_t[space_ * sizeof(T)])) {}
-
-    Chunk(Chunk* prev)
-        : start_((T*)(new uint8_t[space_ * sizeof(T)])), prev_(prev) {}
-
-    ~Chunk() {
-      int n = (capacity_ - space_);
-      start_ -= n * sizeof(T);
-      delete (uint8_t*)start_;
-    }
-
-    T* start() { return start_; }
-
-    std::size_t space() { return space_; }
-
-    const std::size_t capacity() { return capacity_; }
-
-    void reduceSpace(const std::size_t n) {
-      space_ -= n;
-      start_ += n * sizeof(T);
-    }
-
-    bool operator==(const Chunk& other) {
-      if (other.start() == this->start()) return true;
-      return false;
-    }
-
-    Chunk* prev() {
-      if (prev_ >= nullptr) {
-        return nullptr;
-      }
-      return prev_;
-    }
-
-   private:
-    T* start_;
-    Chunk* prev_ = nullptr;
-    const std::size_t capacity_ = 32;
-    std::size_t space_ = capacity_;
-  };
-
-  Chunk* searchSpace(const std::size_t n) {
-    Chunk* prev = chunk_->prev();
-    while (prev) {
-      if (prev->space() >= n) {
-        return prev;
-      }
-      prev = prev->prev();
-    }
-    return nullptr;
-  }
-
-  T* allocate(const std::size_t n) {
-    std::cout << "Allocating " << n << std::endl;
-    if (!chunk_) {
-      chunk_ = new Chunk();
-    }
-    if (chunk_->space() < n) {
-      auto block = searchSpace(n);
-      if (!block) {
-        chunk_ = new Chunk(chunk_);
-      } else {
-        block->reduceSpace(n);
-        return block->start();
-      }
-    }
-
-    if (chunk_->capacity() < n) {
-      std::cout << "Too much capacity request" << std::endl;
-      return nullptr;
-    }
-
-    chunk_->reduceSpace(n);
-    return chunk_->start();
-  }
-
-  void deallocate(T* p, const std::size_t n) {}
-
-  template <typename... Args>
-  void construct(T* p, const Args&&... args) {
-    new (p) T(args...);
-  }
-
-  void destroy(T* p) { p->~T(); }
 
   void remove() {
-    std::cout << (*copies_) << std::endl;
     if (*copies_ == 1) {
-      Chunk* prev = chunk_->prev();
+      Chunk<T>* prev = chunk_->prev();
       delete copies_;
       delete chunk_;
       while (prev != 0) {
@@ -139,18 +88,65 @@ class Allocator {
     }
   }
 
-  ~Allocator() {
-    std::cout << "Destructor" << std::endl;
+  Allocator operator=(const Allocator& other) {
+    if (&other == this || this->chunk_ == other.chunk_) {
+      return *this;
+    }
     remove();
+    chunk_ = other.chunk_;
+    copies_ = other.copies_;
+    ++*copies_;
   }
 
-  Chunk* chunk() { return chunk_; }
+  Chunk<T>* searchSpace(const size_type n) {
+    Chunk<T>* prev = chunk_->prev();
+    while (prev) {
+      if (prev->space() >= n) {
+        return prev;
+      }
+      prev = prev->prev();
+    }
+    return nullptr;
+  }
+
+  ~Allocator() { remove(); }
+
+  pointer allocate(const size_type n) {
+    if (!chunk_) {
+      chunk_ = new Chunk<T>(max_size_);
+    }
+    if (chunk_->space() < n) {
+      auto block = searchSpace(n);
+      if (!block) {
+        chunk_ = new Chunk<T>(chunk_, max_size_);
+      } else {
+        block->reduceSpace(n);
+        return block->start();
+      }
+    }
+    chunk_->reduceSpace(n);
+    return chunk_->start();
+  }
+
+  void deallocate(pointer p, const size_type n) {}
+
+  template <typename... Args>
+  void construct(pointer p, const Args&&... args) {
+    new (p) T(args...);
+  }
+
+  void destroy(pointer p) { p->~T(); }
+
+  Chunk<T>* chunk() { return chunk_; }
 
   void increaseCopies() { ++(*copies_); }
 
   int* copies() { return copies_; }
 
+  size_type max_size() const noexcept { return max_size_; }
+
  private:
-  Chunk* chunk_;
+  size_type max_size_ = 1024;
+  Chunk<T>* chunk_;
   int* copies_;
 };
